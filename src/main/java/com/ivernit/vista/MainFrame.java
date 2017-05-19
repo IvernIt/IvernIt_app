4579018a10323ea6920befdef3a0b45a11b8de3c
pablolp <pablolomardo@arizmendikide.net>
2017-05-19 10:08:37 +0200

    Login

--- src/main/java/com/ivernit/vista/MainFrame.java
+++ src/main/java/com/ivernit/vista/MainFrame.java
@@ -5,7 +5,9 @@
  */
 package com.ivernit.vista;
 
+import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Toolkit;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
@@ -23,17 +25,20 @@ public class MainFrame extends JFrame{
     }
 
     private void init() {
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);        
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        double screenW = screenSize.getWidth();
+        double screenH = screenSize.getHeight();
+        Container contentPane;
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);    
+        this.setLocation((int)( screenW/2 - xSize/2),((int)(  screenH/2 - ySize/2)));
         this.setPreferredSize(new Dimension(xSize, ySize));
-        this.setLayout(new AbsoluteLayout());
         this.setResizable(false);
         this.setTitle(title);        
-        JPanel login = new PLogin();
-        this.setContentPane(login);   
+        contentPane = this.getContentPane();   
+        contentPane.setLayout(null);
+        contentPane.add( new PLogin(xSize, ySize));
         this.pack();
         this.setVisible(true);
-    }
-    
-    
+    }   
     
 }
--- src/main/java/com/ivernit/vista/PLogin.java
+++ src/main/java/com/ivernit/vista/PLogin.java
@@ -6,24 +6,26 @@
 package com.ivernit.vista;
 
 import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
 
 /**
  *
  * @author Pablo
  */
 public class PLogin extends JPanel {
-    private final int xPos = 213;
-    private final int yPos = 153;
-    private final int xSize = 334;
-    private final int ySize = 190;
+
+    private int xPos = 213;
+    private int yPos = 153;
+    private final int width = 334;
+    private final int height = 190;
     private final String sLogin = "Login";
     private final String sUsuario = "Usuario";
     private final String sContraseña = "Contraseña";
@@ -31,34 +33,43 @@ public class PLogin extends JPanel {
     private final String sRegistrarse = "Registrarse";
     private JTextField tfUsuario;
     private JPasswordField pfContraseña;
-    public PLogin() {
+
+    
+    public PLogin(int parentWidth, int parentHeight) {
+        xPos = parentWidth/2 - width/2;
+        yPos = parentHeight/2 - height/2;
         initComponents();
     }
 
-    private void initComponents() {    
-        JPanel pPrincipal = new JPanel(new GridLayout(2,2));
+    private void initComponents() {
+        
+        JPanel pPrincipal = new JPanel(new GridLayout(2, 2));
         JPanel pTitulo = new JPanel(new FlowLayout());
         JPanel pBotones = new JPanel();
         JLabel lbLogin = new JLabel(sLogin);
         JLabel lbUsuario = new JLabel(sUsuario);
         JLabel lbContraseña = new JLabel(sContraseña);
         JButton bEntrar = new JButton(sEntrar);
-        JButton bRegistrarse = new JButton(sRegistrarse);     
-        tfUsuario = new JTextField(sUsuario);
-        pfContraseña = new JPasswordField(sContraseña);
-        this.setLocation(xPos, yPos);
-        this.setPreferredSize(new Dimension(xSize, ySize));
-        this.setLayout(new BorderLayout());    
-        pTitulo.add(lbLogin);
+        JButton bRegistrarse = new JButton(sRegistrarse);
+        tfUsuario = new JTextField();
+        pfContraseña = new JPasswordField();
+        this.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.LOWERED));
+        this.setBounds(xPos, yPos, width, height);
+        this.setLayout(new BorderLayout());
+        pTitulo.setLayout(new BorderLayout());
+        pTitulo.setBorder(BorderFactory.createEtchedBorder(EtchedBorder.RAISED));
+        pTitulo.add(lbLogin,BorderLayout.NORTH);
         pBotones.add(bRegistrarse);
         pBotones.add(bEntrar);
-        pPrincipal.add(lbUsuario);
-        pPrincipal.add(tfUsuario);
-        pPrincipal.add(lbContraseña);
-        pPrincipal.add(pfContraseña);
+        pPrincipal.setBorder(BorderFactory.createEmptyBorder(30, 20, 10, 20));
+        pPrincipal.add(new AuxPane(lbUsuario));
+        pPrincipal.add(new AuxPane(tfUsuario));
+        pPrincipal.add(new AuxPane(lbContraseña));
+        pPrincipal.add(new AuxPane(pfContraseña));
         this.add(pTitulo, BorderLayout.NORTH);
         this.add(pPrincipal, BorderLayout.CENTER);
         this.add(pBotones, BorderLayout.SOUTH);
+
     }
-    
+
 }
